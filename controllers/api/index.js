68e9e261a61720a0f1303dfd0812ebc63d4688db
MWandall <144394104+MWandall@users.noreply.github.com>
2023-11-02 00:03:36 -0500

    starting work on user, post, and home routes in controllers. a few issues with spread opperators. W.I.P

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
-const exampleRoutes = require("./exampleRoutes");
-const exampleRoutes2 = require("./exampleRoutes2");
+const userRoutes = require("./userRoutes");
+const postRoutes = require("./postRoutes");
+const commentRoutes = require("./commentRoutes");
 
-router.use("/users", exampleRoutes);
-router.use("/posts", exampleRoutes2);
+
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
