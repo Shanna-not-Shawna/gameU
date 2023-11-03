473e460db906af5e74101d2cabc40d63b7da069f
MWandall <144394104+MWandall@users.noreply.github.com>
2023-11-03 13:58:40 -0500

    added dynamic info to views

--- models/index.js
+++ models/index.js
@@ -4,31 +4,48 @@ const Post = require("./Post");
 const Game = require("./Game");
 const Comment = require("./Comment");
 
+
+
+// Post.belongsTo(User, {
+//   foreignKey: "user_id"
+// });
+
+// Post.hasMany(Comment, {
+//   foreignKey: "post_id",
+//   onDelete: "CASCADE"
+// });
+
+// Comment.belongsTo(Post, {
+//   foreignKey: "post_id",
+// });
+
+// User.hasMany(Comment, {
+//   foreignKey: "user_id",
+//   onDelete: "CASCADE"
+// });
+
+// Comment.belongsTo(User, {
+//   foreignKey: "user_id"
+// });
+
 User.hasMany(Post, {
   foreignKey: "user_id",
   onDelete: "CASCADE"
 });
 
 Post.belongsTo(User, {
-  foreignKey: "user_id"
-});
-
-Post.hasMany(Comment, {
-  foreignKey: "post_id",
+  foreignKey: "userId",
   onDelete: "CASCADE"
 });
 
-Comment.belongsTo(Post, {
-  foreignKey: "post_id",
-});
-
-User.hasMany(Comment, {
-  foreignKey: "user_id",
+Post.hasMany(Comment, {
+  foreignKey: "postId",
   onDelete: "CASCADE"
 });
 
 Comment.belongsTo(User, {
-  foreignKey: "user_id"
+  foreignKey: "userId",
+  onDelete: "CASCADE"
 });
 
 Game.hasMany(Post, {
