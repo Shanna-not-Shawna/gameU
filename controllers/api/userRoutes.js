cc04937a05e4ddafd384f48b494f7ced562f0098
MWandall <144394104+MWandall@users.noreply.github.com>
2023-11-03 10:18:28 -0500

    added signup.js, made edits to handlebars to handle login, added a post page.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,13 +5,17 @@ const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
   try {
-    const userData = await User.create(req.body);
-
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
     req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
+      req.session.userId = newUser.id;
+      req.session.name = newUser.name;
+      req.session.email = newUser.email;
+      req.session.loggedIn = true;
+      res.json(newUser);
     });
   } catch (err) {
     res.status(400).json(err);
@@ -20,12 +24,12 @@ router.post("/", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { name: req.body.name } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: "Incorrect username or password, please try again" });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
@@ -34,7 +38,7 @@ router.post("/login", async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: "Incorrect name or password, please try again" });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
@@ -47,6 +51,7 @@ router.post("/login", async (req, res) => {
 
   } catch (err) {
     res.status(400).json(err);
+    console.log(err);
   }
 });
 
