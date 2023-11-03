8ce10a913d2c78be13a6f7f8954b7c6ac2f74c30
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-11-03 13:58:38 -0500

    function game api search fixed and minor styling added

--- public/js/rawgdata.js
+++ public/js/rawgdata.js
@@ -1,53 +1,58 @@
-apiKey = 758f0fb8bc1741f39e2f3777d1996684;
+apiKey = '758f0fb8bc1741f39e2f3777d1996684';
 const apiUrl = 'https://rawg.io/api/games?token&key=';
-const url = `${apiUrl}${apiKey}${numRes}`;
 const rating = '&metacritic=97,100';
-const url2 = `${apiUrl}${apiKey}${rating}${numRes}`;
 const numRes = '&page_size=10';
+const url = `${apiUrl}${apiKey}${numRes}`;
+const url2 = `${apiUrl}${apiKey}${rating}${numRes}`;
 
 // top 10 query
 fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
         const slug = response.slug;
         const title = response.slug;
         const image = response.background_image;
 
 
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
     });
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
 
-// RAWG title search
-fetch(url2)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-       
+function gameSearch(event) {
+    event.preventDefault()
+    // RAWG title search
+    const gameInput = document.querySelector('#game-query').value
+    fetch(`https://api.rawg.io/api/games?key=758f0fb8bc1741f39e2f3777d1996684&page_size=10&search=${gameInput}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
 
 
-    });
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
-
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+document.querySelector('#game-search-form').addEventListener('submit', gameSearch)
 // API calls: 
 // 1) top 8-10 games based on metacritic rating
-        // need slug, name and background_image url string
+// need slug, name and background_image url string
 
-        // https://api.rawg.io/api/games?key=758f0fb8bc1741f39e2f3777d1996684&page_size=10&metacritic=97,100
+// https://api.rawg.io/api/games?key=758f0fb8bc1741f39e2f3777d1996684&page_size=10&metacritic=97,100
 
 // 2) search for a title on RAWG via search box on homepage
-        // need slug, name and background_image url string
+// need slug, name and background_image url string
 
-        // https://api.rawg.io/api/games?key=758f0fb8bc1741f39e2f3777d1996684&page_size=10&search=%22mario%22
+// https://api.rawg.io/api/games?key=758f0fb8bc1741f39e2f3777d1996684&page_size=10&search=%22mario%22
