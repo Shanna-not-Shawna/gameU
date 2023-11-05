cd16c9256ce6d0b19ade40036615c56892d93232
austintynes <austintynes10@gmail.com>
2023-11-04 23:06:04 -0500

    rough idea styles for login, create, and homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,21 +1,13 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const postRoutes = require("./postRoutes");
-<<<<<<< HEAD
-//const commentRoutes = require("./commentRoutes");
-=======
 const commentRoutes = require("./commentRoutes");
 const gameRoutes = require("./gameRoutes");
->>>>>>> 1bfad112c71bd26fbff9314ed1ee8435dd1e4d98
 
 
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
-<<<<<<< HEAD
-//router.use("/comments", commentRoutes);
-=======
 router.use("/comments", commentRoutes);
 router.use("/games", gameRoutes);
->>>>>>> 1bfad112c71bd26fbff9314ed1ee8435dd1e4d98
 
 module.exports = router;
